e3b24f4ac742300783176db339bcc36336ae9195
Mohammad Aarish <mohdaarish127@gmail.com>
2025-10-06 00:07:50 +0530

    changed connection string

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ if (!process.env.ACCESS_TOKEN) {
   process.exit(1);
 }
 mongoose
-  .connect(config.connectionString)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
